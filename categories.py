7f4dbd1619ab64288205518f6a3609db16830cb3
Raksha Karn <karnraksha41@gmail.com>
2026-02-17 01:57:10 +0545

    Add view categories with or without filtering

--- categories.py
+++ categories.py
@@ -35,10 +35,35 @@ class CategoryManager:
             print(f"{category_type} category added successfully!")
 
     def view_all_categories(self):
-        pass
+        print("\n|   Type    |      Category       |")
+        print("|-----------|---------------------|")
+
+        if not self.categories["Income"]:
+            print(f"| {'Income':<9} | {'No categories':<19} |")
+
+        if not self.categories["Expense"]:
+            print(f"| {'Expense':<9} | {'No categories':<19} |")
+
+        for category in self.categories["Income"]:
+            print(f"| {'Income':<9} | {category:<19} |")
+
+        for category in self.categories["Expense"]:
+            print(f"| {'Expense':<9} | {category:<19} |")
+
+        print("|-----------|---------------------|")
 
     def view_categories_by_type(self, category_type):
-        pass
+        print(f"\n|   {category_type} Categories   |")
+        print("|-------------------------------|")
+
+        if not self.categories[category_type]:
+            print("|        No categories          |")
+        else:
+            for category in self.categories[category_type]:
+                print(f"| {category:<29} |")
+
+        print("|-------------------------------|")
+
 
     def remove_categories(self):
         print("Take a look at all of the categories! ")
@@ -54,10 +79,15 @@ class CategoryManager:
             category_type = "Income" if choice == "1" else "Expense"
             
             self.view_categories_by_type(category_type)
+            if not self.categories[category_type]:
+                print("No categories to remove.")
+                continue
+
             while True: 
                 category = input("Enter category to remove: ").title()
                 if category in self.categories[category_type]:
                     self.categories[category_type].remove(category)
+                    print("Category removed successfully!")
                     break
                 else:
                     print("Category does not exist!")
