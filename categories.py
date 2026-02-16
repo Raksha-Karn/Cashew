a86db1eb96a744175179a7d1d919f7c98528fb3c
Raksha Karn <karnraksha41@gmail.com>
2026-02-17 01:35:48 +0545

    Modify add categories and Add remove categories

--- categories.py
+++ categories.py
@@ -1,32 +1,64 @@
-categories = {
-    "Income": [],
-    "Expense": []
-}
+class CategoryManager:
+    def __init__(self):
+        self.categories = {
+            "Income": [],
+            "Expense": []
+        }
 
-def add_categories():
-    while True:
-        choice = input(
-            "\nEnter:\n"
-            "1. Income Type\n"
-            "2. Expense Type\n"
-            "3. Exit\n"
-            "Choice: "
-        )
+    def add_categories(self):
+        while True:
+            choice = input(
+                "\nEnter:\n"
+                "1. Add Income Category\n"
+                "2. Add Expense Category\n"
+                "3. Exit\n"
+                "Choice: "
+            )
 
-        if choice == "3":
-            break
+            if choice == "3":
+                break
 
-        if choice not in ["1", "2"]:
-            print("Invalid input. Try again.")
-            continue
+            if choice not in ["1", "2"]:
+                print("Invalid input. Try again.")
+                continue
 
-        category_type = "Income" if choice == "1" else "Expense"
+            category_type = "Income" if choice == "1" else "Expense"
 
-        category = input(f"Enter {category_type} Category: ")
+            example = "salary, stipend" if choice == "1" else "food, rent"
+            category = input(f"Enter {category_type} Category (ex. {example}): ").title()
 
-        if category in categories[category_type]:
-            print("Category already exists!")
-            continue
+            if category in self.categories[category_type]:
+                print("Category already exists!")
+                continue
 
-        categories[category_type].append(category)
-        print(f"{category_type} category added successfully!")
+            self.categories[category_type].append(category)
+            print(f"{category_type} category added successfully!")
+
+    def view_all_categories(self):
+        pass
+
+    def view_categories_by_type(self, category_type):
+        pass
+
+    def remove_categories(self):
+        print("Take a look at all of the categories! ")
+        self.view_all_categories()
+        while True:
+            choice = input("Enter type of category to remove from: \n1.Income\n2.Expense\n3.Exit")
+            if choice == "3":
+                break
+            if choice not in ["1", "2", "3"]:
+                print("Invalid input. Try again.")
+                continue
+
+            category_type = "Income" if choice == "1" else "Expense"
+            
+            self.view_categories_by_type(category_type)
+            while True: 
+                category = input("Enter category to remove: ").title()
+                if category in self.categories[category_type]:
+                    self.categories[category_type].remove(category)
+                    break
+                else:
+                    print("Category does not exist!")
+            
